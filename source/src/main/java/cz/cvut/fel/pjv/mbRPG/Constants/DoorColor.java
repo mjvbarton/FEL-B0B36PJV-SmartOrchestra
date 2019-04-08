b8cf8e70cb3f5a0d277954e4f49269ca1ba01379
Matěj Bartoň <bartom47@fel.cvut.cz>
2019-04-09 00:22:47 +0200

    Class Model - CONSTANTS

---
+++ source/src/main/java/cz/cvut/fel/pjv/mbRPG/Constants/DoorColor.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cz.cvut.fel.pjv.mbRPG.Constants;
+
+/**
+ *
+ * @author Matěj Bartoň
+ */
+public enum DoorColor {
+    RED,
+    GREEN,
+    BLUE;
+}
