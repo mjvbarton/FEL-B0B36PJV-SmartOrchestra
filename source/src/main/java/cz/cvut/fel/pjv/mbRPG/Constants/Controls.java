83ce4cd3d3c6f70d5dc705313388572f7b0935a4
Matěj Bartoň <bartom47@fel.cvut.cz>
2019-04-09 05:06:30 +0200

    Class Model - Constants - Added 'Controls.java'

---
+++ source/src/main/java/cz/cvut/fel/pjv/mbRPG/Constants/Controls.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cz.cvut.fel.pjv.mbRPG.Constants;
+
+/**
+ *
+ * @author Matěj Bartoň
+ */
+public enum Controls{
+    LEFT,
+    RIGHT,
+    JUMP,
+    EXIT,
+    ATTACK;
+}
