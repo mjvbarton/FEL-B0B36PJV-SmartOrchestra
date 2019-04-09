9ebaf1cf9cce915fd4f5a745098df6665c5cb924
Matěj Bartoň <bartom47@fel.cvut.cz>
2019-04-09 05:07:25 +0200

    Class Model - Controllers - Added 'MonsterControl' & 'PlayerControl'

--- source/src/main/java/cz/cvut/fel/pjv/mbRPG/Main.java
+++ source/src/main/java/cz/cvut/fel/pjv/mbRPG/Main.java
@@ -5,17 +5,26 @@
  */
 package cz.cvut.fel.pjv.mbRPG;
 
+import javafx.application.Application;
+import javafx.stage.Stage;
+
 /**
  * The main class of the whole project
  * @author Matěj Bartoň
  */
-public class Main {
+public class Main extends Application {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        
     }
     
 }
--- source/src/main/java/cz/cvut/fel/pjv/mbRPG/PlayerControl.java
+++ source/src/main/java/cz/cvut/fel/pjv/mbRPG/PlayerControl.java
@@ -5,10 +5,46 @@
  */
 package cz.cvut.fel.pjv.mbRPG;
 
+import cz.cvut.fel.pjv.mbRPG.Constants.Controls;
+import cz.cvut.fel.pjv.mbRPG.Constants.Move;
+import cz.cvut.fel.pjv.mbRPG.model.LivingEntity;
+
 /**
  *
  * @author Matěj Bartoň
+ * @param <T> Living object
+ * @param <C>
  */
-public class PlayerControl {
-
+public class PlayerControl<T extends LivingEntity> implements Runnable{
+    private final T player;
+    
+    public PlayerControl(T player){
+        this.player = player;
+    }
+    
+    /**
+     * Controller workflow
+     */
+    @Override
+    public void run() {
+        
+    }
+    
+    /**
+     * Perform step towards the selected direction
+     * 
+     * @param direction 
+     */
+    private void step(Move direction){
+        
+    }
+    
+    /**
+     * Do selected action
+     * 
+     * @param control 
+     */
+    public void action(Controls control){
+       
+    }
 }
