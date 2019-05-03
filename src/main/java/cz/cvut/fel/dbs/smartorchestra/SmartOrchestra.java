4eb5a1650f5265878c5e4ed2fafabecce175a54a
Matěj Bartoň <bartom47@fel.cvut.cz>
2019-05-03 20:18:34 +0200

    UC 01 - Basic, No further implementation

--- src/main/java/cz/cvut/fel/dbs/smartorchestra/SmartOrchestra.java
+++ src/main/java/cz/cvut/fel/dbs/smartorchestra/SmartOrchestra.java
@@ -5,17 +5,40 @@
  */
 package cz.cvut.fel.dbs.smartorchestra;
 
+import cz.cvut.fel.dbs.smartorchestra.gui.LoginScreen;
+import cz.cvut.fel.dbs.smartorchestra.model.UserManager;
+import javax.persistence.*;
+import org.mindrot.jbcrypt.BCrypt;
+
+
 /**
  *
  * @author Matěj Bartoň
  */
 public class SmartOrchestra {
-
+    private static EntityManager em;
+    private static EntityManagerFactory emf;
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        setupJPA();
+        
+        LoginScreen login = new LoginScreen();
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                login.setVisible(true);
+            }
+        });
     }
     
+    public static final EntityManager getEntityManager(){
+        return em;
+    }
+    
+    private static final void setupJPA(){
+        emf = Persistence.createEntityManagerFactory("smartorchestraPU");
+        em = emf.createEntityManager();
+    }
 }
