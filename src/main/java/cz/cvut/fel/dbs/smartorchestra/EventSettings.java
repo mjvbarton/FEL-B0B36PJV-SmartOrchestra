0900ebb655c11ba398ba752e5fc78dd5ff40e52b
Matěj Bartoň <bartom47@fel.cvut.cz>
2019-05-11 23:22:41 +0200

    Created UI for UC32, UC33

---
+++ src/main/java/cz/cvut/fel/dbs/smartorchestra/EventSettings.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cz.cvut.fel.dbs.smartorchestra;
+
+import cz.cvut.fel.dbs.smartorchestra.gui.EventDetails;
+
+/**
+ *
+ * @author Matěj Bartoň
+ */
+public class EventSettings {
+
+    public EventSettings(EventDetails aThis) {
+        
+    }
+    
+}
