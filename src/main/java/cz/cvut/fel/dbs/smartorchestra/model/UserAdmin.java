c03b2487d45f35e47329e3b5e807a6ff1b942b84
Matěj Bartoň <bartom47@fel.cvut.cz>
2019-05-20 21:30:28 +0200

    JavaDoc - Model

--- src/main/java/cz/cvut/fel/dbs/smartorchestra/model/UserAdmin.java
+++ src/main/java/cz/cvut/fel/dbs/smartorchestra/model/UserAdmin.java
@@ -14,16 +14,24 @@ import java.util.List;
 import javax.persistence.NoResultException;
 
 /**
- *
- * @author Matěj Bartoň
+ * This class represents model for administrating users. It is an extension of {@link UserManager}
+ * @author Matěj Bartoň <i>(bartom47@fel.cvut.cz)</i>
  */
 public class UserAdmin extends UserManager{
     private List<Users> adminedUsers;
     
+    /**
+     * Creates new UserAdmin
+     */
     public UserAdmin(){
         adminedUsers = new ArrayList();
     }
     
+    /**
+     * Loads users for administraion and saves them to the class property.
+     * @return a {@code List} of {@link Users} entities
+     * @throws UserAdminException when no user was found
+     */
     public List<Users> loadUsers() throws UserAdminException{
         UserReader ur = new UserReader();
         adminedUsers = ur.loadUsers();
@@ -33,10 +41,21 @@ public class UserAdmin extends UserManager{
         return adminedUsers;
     }
     
+    /**
+     * Gets user from and administration based on array index.
+     * @param arrayIndex - {@code int} index in {@code List} of admined users
+     * @return a {@link Users} entity from list of admined users
+     */
     public Users getUserForAdministration(int arrayIndex){
         return adminedUsers.get(arrayIndex);
     }
     
+    /**
+     * Checks if there is no email in the database for given email.
+     * @param email - {@code String} email address
+     * @param activeUser - an {@link Users} entity
+     * @throws UserAdminException there is email in the database
+     */
     public void checkFreeEmail(String email, Users activeUser) throws UserAdminException{
         UserReader ur = new UserReader();
         try{
@@ -48,6 +67,13 @@ public class UserAdmin extends UserManager{
         }
     }
 
+    /**
+     * Sets given user to have administrator permissions. If given boolean value is admin is {@code true}
+     * the given is user is given administrator rights, otherwise the rights from given user are removed.
+     * @param user - a {@code User} entity
+     * @param isAdmin - {@code boolean} value,  
+     * @throws Exception if the process fails on the DAO level
+     */
     public void setAdministrator(Users user, boolean isAdmin) throws Exception{
         AdministratorHandler ah = new AdministratorHandler();
         boolean wasAdmin = checkAdministrator(user);
@@ -59,6 +85,12 @@ public class UserAdmin extends UserManager{
         }
     }    
 
+    /**
+     * Deletes given user and all dependencies from the database.
+     * @param user - a {@link Users} entity to be deleted
+     * @throws UserAdminException when dependent {@link UserWriter} throws it.
+     * @throws Exception when dependent {@link UserWriter} throws it
+     */
     public void removeUser(Users user) throws UserAdminException, Exception{
         UserWriter uw = new UserWriter();
         uw.removeUser(user);
--- src/main/java/cz/cvut/fel/dbs/smartorchestra/model/UserManager.java
+++ src/main/java/cz/cvut/fel/dbs/smartorchestra/model/UserManager.java
@@ -4,43 +4,56 @@
  */
 package cz.cvut.fel.dbs.smartorchestra.model;
 
-import cz.cvut.fel.dbs.smartorchestra.UserSettings;
 import cz.cvut.fel.dbs.smartorchestra.exceptions.*;
 import cz.cvut.fel.dbs.smartorchestra.model.dao.AdministratorHandler;
 import cz.cvut.fel.dbs.smartorchestra.model.dao.UserReader;
 import cz.cvut.fel.dbs.smartorchestra.model.dao.UserWriter;
 import cz.cvut.fel.dbs.smartorchestra.model.entities.Users;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.persistence.NoResultException;
 import org.mindrot.jbcrypt.BCrypt;
 
 
 /**
- *
- * @author Matěj Bartoň
+ * This class is model for basic working with {@link Users} entities.
+ * @author Matěj Bartoň <i>(bartom47@fel.cvut.cz)</i>
  */
 public class UserManager {
     private UserReader ur;
     
+    /**
+     * Creates new UserManager
+     */
     public UserManager(){
         ur = new UserReader();
     }
-    public final Users login(String userMail, String passwd) throws UserManagerException, Exception{
+    
+    /**
+     * This method represents the login process.
+     * @param userMail - {@code String} email address to identify the user
+     * @param passwd - {@code String} uncrypted value of the users password
+     * @return a {@link Users} entity to be set as logged in user
+     * @throws NonExistingUserException when the password is incorrect or the user with given email does not exist.
+     * @throws Exception if the process failed from other reasons
+     */
+    public final Users login(String userMail, String passwd) throws NonExistingUserException, Exception{
         try{
             Users user = ur.getUserFromEmail(userMail);
             if(BCrypt.checkpw(passwd, user.getPasswd()) != true){
                 throw new NonExistingUserException();
             }
             return user;            
-        } catch(UserManagerException err){
+        } catch(UserManagerException | NoResultException err){
             throw new NonExistingUserException();
             
-        } catch(NoResultException err){
-            throw new NonExistingUserException();
         } 
     }
-        
+    
+    /**
+     * Sets given user a new password according to the password given.
+     * @param user - a {@link Users} entity of which the password will be changed
+     * @param NewPasswd - an uncrypted {@code String} object of the new password
+     * @throws UserManagerException when the process fails
+     */
     public final void changePasswd(Users user, String NewPasswd) throws UserManagerException{
         try {
             user.setPasswd(BCrypt.hashpw(NewPasswd, BCrypt.gensalt(12)));
@@ -52,6 +65,11 @@ public class UserManager {
         }       
     }
     
+    /**
+     * Checks if given user has administrator rights.
+     * @param user - a {@link Users} entity
+     * @return {@code true} if given user has administrator rights, {@code false} if not.
+     */
     public final boolean checkAdministrator(Users user){
         AdministratorHandler ah = new AdministratorHandler();
         return ah.getIsAdministrator(user);
